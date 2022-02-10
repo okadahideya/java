f8cdf959c07e36c5078a91cff10032971e42d310
okadahideya <hideya400@gmail.com>
2022-02-10 12:05:19 +0900

    java-first-commit

---
+++ java_test/Operand.java
@@ -0,0 +1,9 @@
+package java_test;
+
+public class Operand {
+  public static void main(String[] args){
+    int number = 1 + 1;
+    ++number;
+    System.out.println(number);
+  }
+}
---
+++ java_test/Variable.java
@@ -0,0 +1,9 @@
+package java_test;
+
+public class Variable {
+  public static void main(String[] args) {
+    int myAge;
+    myAge = 28;
+    System.out.println(myAge);
+  }
+}
